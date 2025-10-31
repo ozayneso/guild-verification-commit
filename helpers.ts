a3fc7f99a18586fa14567e9ba8760bf8a70ca94f
neso <96296047+ozayneso@users.noreply.github.com>
2025-10-31 09:35:48 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T06:35:46.515Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-10-31T06:35:47.963Z
+Commit: Resolve null pointer exception in user service
