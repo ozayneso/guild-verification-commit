788d4f564a68e455da6a9f8e64393fb8fb560ec7
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-12 09:11:37 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T06:10:28.693Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-12T06:11:37.017Z
+Commit: Resolve null pointer exception in user service
