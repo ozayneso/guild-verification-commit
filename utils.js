c6c340123969c8ff9ed965afe21a0865a62d6758
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-12 09:14:22 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T06:14:12.740Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-12T06:14:22.878Z
+Commit: Fix memory leak in event handlers
