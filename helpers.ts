2addecf07a5e3e82130e22e0bec0af48f5cd7636
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-12 09:14:34 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T06:14:16.814Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-12T06:14:34.432Z
+Commit: Correct typo in configuration file
