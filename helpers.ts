9ee9de9f002a3cc8034c09e0dba5bb8a5ddb84f8
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-12 09:14:16 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T06:13:29.420Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-12T06:14:16.814Z
+Commit: Fix broken links in documentation
