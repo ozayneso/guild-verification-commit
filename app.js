3ee2fb792b21ac7b1ef2a503db08eef22b069131
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-12 09:14:18 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-12T06:14:05.556Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-12T06:14:18.133Z
+Commit: Fix CSS styling bug on mobile devices
