54faf96feb077ce5b64461c341ffb10128be6a9d
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:16:09 +0300

    Add export functionality for reports

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:15:14.603Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-13T09:16:09.483Z
+Commit: Add export functionality for reports
