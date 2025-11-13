a576a1779c331f80438c7c086491ff7388838b92
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:16:50 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:16:44.141Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-13T09:16:50.452Z
+Commit: Add multi-language support
