e51219d22ae9f24e576f9ff6d72c72eb418ab05f
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:17:05 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:16:37.481Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-13T09:17:05.547Z
+Commit: Fix CSS styling bug on mobile devices
