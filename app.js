200c7e866e71d9eddec407fbc16ffa2b6c41d008
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:36:53 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:36:48.089Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-13T10:36:53.666Z
+Commit: Resolve null pointer exception in user service
