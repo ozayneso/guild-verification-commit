09d26e5fdada0d64a53e3a1bc1903e5d0cbd2da7
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 11:51:24 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T08:51:12.770Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-13T08:51:24.058Z
+Commit: Fix memory leak in event handlers
