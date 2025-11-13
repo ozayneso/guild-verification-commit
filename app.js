617b43803df56e6d99bc86468ef8ab14247a9e22
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:20:27 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:18:18.836Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-13T09:20:27.741Z
+Commit: Fix broken links in documentation
