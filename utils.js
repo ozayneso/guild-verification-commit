e1857233fe83d35d38d00edb5e17b3b147c6956a
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 11:49:20 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T08:44:40.266Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-13T08:49:20.755Z
+Commit: Add export functionality for reports
