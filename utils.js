6300a94e08d9f76ec99b4afac8226206ef456bb3
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:32:49 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:32:43.232Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-13T10:32:49.071Z
+Commit: Resolve timezone issues in date handling
