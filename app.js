1977a877982e97492ebb285ef50d3bc55215d477
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:24:38 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:24:32.598Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-13T10:24:38.286Z
+Commit: Resolve null pointer exception in user service
