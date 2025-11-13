2563ab76cce747ce10ca4b507c962c18e52b2c27
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:23:17 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:22:53.393Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-13T10:23:16.997Z
+Commit: Fix broken links in documentation
