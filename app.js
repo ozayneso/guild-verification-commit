653788a91a4a87cb4ee6bd69879dc07d4907fac3
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:25:35 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:25:32.544Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-13T10:25:35.320Z
+Commit: Fix memory leak in event handlers
