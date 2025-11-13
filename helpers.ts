eaeb2221e96bb9f785d77cc2a14226595708b076
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:23:47 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:23:18.277Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-13T10:23:47.299Z
+Commit: Patch API endpoint error handling
