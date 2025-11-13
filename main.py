41d1423b5b8ba1469e676733a5ece53b1904abff
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:17:32 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:17:28.805Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-13T09:17:32.099Z
+Commit: Fix race condition in async operations
