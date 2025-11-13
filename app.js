f3c36776aeacfb57f3c103a16e3d6138f259290f
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:21:32 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:21:08.917Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-13T09:21:32.049Z
+Commit: Patch API endpoint error handling
