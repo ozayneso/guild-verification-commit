a89df6084c30d627eec5a9376a0732a094ce1461
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:12:19 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:12:07.715Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-13T09:12:19.451Z
+Commit: Correct typo in configuration file
