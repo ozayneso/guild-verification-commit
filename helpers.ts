fd216f800d42bcd3fbc0f24cf92b1e18542bf37d
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:31:36 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:31:10.885Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-13T10:31:36.573Z
+Commit: Fix memory leak in event handlers
