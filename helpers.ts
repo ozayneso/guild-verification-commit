6c7dbf304c52c104dc78dc231f284f18a2f69d4d
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:33:02 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:32:46.296Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-13T10:33:01.968Z
+Commit: Introduce real-time notifications
