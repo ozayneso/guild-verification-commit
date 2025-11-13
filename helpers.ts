2f600e2b972303f8ad8749b18ed700bc8deeeb62
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:20:31 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:18:14.300Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-13T09:20:31.613Z
+Commit: Patch API endpoint error handling
