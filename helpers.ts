afa989ee39c04a1742b9261f2d92ffbcb860b588
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:25:22 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:25:12.713Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-13T10:25:22.552Z
+Commit: Correct typo in configuration file
