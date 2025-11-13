1481e31f9c35e9ed222fa43d71440d0db3d8d97b
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:29:07 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:28:59.319Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-13T10:29:06.897Z
+Commit: Introduce real-time notifications
