fbba495de43a355062f055182070fdb5cbb0769f
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 11:49:49 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T08:49:36.620Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-13T08:49:49.827Z
+Commit: Patch security vulnerability in authentication
