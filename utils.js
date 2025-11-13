cb70df1129cfa990ea42c9b5f5173475f767ab0b
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:33:52 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:33:39.244Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-13T10:33:52.362Z
+Commit: Patch API endpoint error handling
