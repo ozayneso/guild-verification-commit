549d78679c238a42b4fb4abbb48a733434807836
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:34:30 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:34:25.933Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-13T10:34:30.133Z
+Commit: Fix broken links in documentation
