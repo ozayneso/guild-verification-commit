231b2573dfc104d67e8930eef7967049802e1862
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:27:28 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:27:16.213Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-13T10:27:28.037Z
+Commit: Resolve null pointer exception in user service
