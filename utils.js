2cd5adebdf707f93c55b476fde41c6d237f7fb55
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:33:24 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:32:59.008Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-13T10:33:23.946Z
+Commit: Fix CSS styling bug on mobile devices
