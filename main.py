ed18bd23dcab5e97fb992478611aad89d92f0273
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:34:04 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:33:13.626Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-13T10:34:04.730Z
+Commit: Fix CSS styling bug on mobile devices
