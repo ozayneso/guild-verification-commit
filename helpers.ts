976a715eb82154cf15c45033d957793b756c8383
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:27:25 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:27:20.724Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-13T10:27:25.285Z
+Commit: Resolve timezone issues in date handling
