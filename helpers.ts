62788796a80e352576cebef81bf96f9fcb2d689a
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:16:31 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:16:29.813Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-13T09:16:31.133Z
+Commit: Resolve null pointer exception in user service
