14cfcf1477e0ce03491b5589bf3e5eb749e88dc2
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:25:20 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:25:17.176Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-13T10:25:19.896Z
+Commit: Fix CSS styling bug on mobile devices
