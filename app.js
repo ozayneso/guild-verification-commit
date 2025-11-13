e58ca11366900c3e18746535e220236a90696d91
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:26:33 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:26:22.370Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-13T10:26:32.978Z
+Commit: Resolve null pointer exception in user service
