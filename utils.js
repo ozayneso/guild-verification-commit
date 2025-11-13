ba97d8f0c4598f909528dbf69abfbd8f4d306102
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:27:00 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:26:31.657Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-13T10:27:00.786Z
+Commit: Fix race condition in async operations
