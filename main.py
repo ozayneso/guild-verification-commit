3190fd7773ca8635bbf04fe18194a77952bf7e09
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:14:46 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:14:07.707Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-13T09:14:46.196Z
+Commit: Resolve null pointer exception in user service
