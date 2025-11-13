dbae7a6f73f3dd1c7c3ee717c6eb118bdcd10b7c
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:18:18 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:18:12.605Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-13T09:18:18.836Z
+Commit: Fix memory leak in event handlers
