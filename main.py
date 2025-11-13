c1bffeb3f5d1bd3a94061731697ad7a7dcf25976
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:14:04 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:13:42.395Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-13T09:14:04.498Z
+Commit: Fix memory leak in event handlers
