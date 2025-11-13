35959ab9b9e80cd24e856f62285234852f30a023
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:32:18 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:31:28.598Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-13T10:32:18.024Z
+Commit: Fix broken links in documentation
