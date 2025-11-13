6dec3e79e967f79bdc214b3c43a276e77547413d
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:14:12 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:13:34.547Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-13T09:14:12.683Z
+Commit: Fix broken links in documentation
