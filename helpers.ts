55322174f20f72100b643746657080ec57b09e0d
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:33:25 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:33:19.786Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-13T10:33:25.227Z
+Commit: Fix memory leak in event handlers
