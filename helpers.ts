f0cb63c0ff6fa486113903f938091e4a1187f90e
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:36:43 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:36:13.729Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-13T10:36:43.049Z
+Commit: Implement user profile customization
