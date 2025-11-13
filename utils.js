0e4d683197b77083a14add4238b7bec565d996f6
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:24:52 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:24:50.870Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-13T10:24:52.406Z
+Commit: Patch API endpoint error handling
