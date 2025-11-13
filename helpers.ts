09ba9a0a5281f86429a07e713a4370292f0736b3
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:16:08 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:15:24.501Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-13T09:16:07.940Z
+Commit: Correct typo in configuration file
