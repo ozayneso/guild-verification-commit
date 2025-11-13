09e1d8debb762cb812134ae587141ba1e6f1e0d9
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:17:17 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:17:06.988Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-13T09:17:17.557Z
+Commit: Patch API endpoint error handling
