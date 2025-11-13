424e3e6dd700df7cc700af22bd12b73ee1920345
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:34:26 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:34:04.730Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-13T10:34:25.933Z
+Commit: Introduce real-time notifications
