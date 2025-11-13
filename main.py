2d0981859bd67814dbb4ab7bb34302e0ec8abf82
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:12:32 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:11:57.301Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-13T09:12:32.628Z
+Commit: Fix race condition in async operations
