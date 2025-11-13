4e7ee818b720be54a360c35ab6b163a4acec053c
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 12:17:07 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T09:17:05.547Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-13T09:17:06.988Z
+Commit: Fix broken links in documentation
