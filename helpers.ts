b99bafa81afd9c94b6604b941e38c8232bfbabd2
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:36:44 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:36:43.049Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-13T10:36:44.289Z
+Commit: Fix race condition in async operations
