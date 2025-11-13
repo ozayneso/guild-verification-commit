1fae1775462536b49d3d8d77fe15a9f8ca53feb7
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:36:28 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:34:52.148Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-13T10:36:27.945Z
+Commit: Fix CSS styling bug on mobile devices
