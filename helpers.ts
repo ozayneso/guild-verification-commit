6f049231cf1eca0a1134e099653edd5e69fd6f38
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-13 13:33:48 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-13T10:33:46.794Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-13T10:33:48.155Z
+Commit: Correct typo in configuration file
