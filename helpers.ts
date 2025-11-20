1f495c938801e341bc7c2e9365cfdd29d848a4a9
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-20 10:15:27 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-20T07:13:55.489Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-20T07:15:28.088Z
+Commit: Resolve null pointer exception in user service
