7f36de7944d34468f6f963a11ef5bbdab18fcce6
neso <96296047+ozayneso@users.noreply.github.com>
2025-11-25 11:36:43 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-20T07:15:28.088Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-25T08:36:42.956Z
+Commit: Patch security vulnerability in authentication
